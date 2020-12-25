0855a10f3ea1a6dfe13a491ac61aee4c9ef3a0c1
bvsspavankumar <bvsspavankumar@gmail.com>
2020-12-25 09:18:42 +0530

    Added navigation to client. Closes #3

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,20 @@ import { Switch, Route } from 'react-router-dom'
 import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
 import Home from './pages/Home'
+import Header from './components/nav/Header'
 
 import './App.css';
 
 const App = () => {
   return (
-    <div>
-    <div>Index</div>
+    <>
+    <Header />
     <Switch>
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
       <Route path="/" component={Home} />
     </Switch>
-    </div>
+    </>
   );
 }
 
