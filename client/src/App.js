9f1de2746bdf2675930b22769d4b161fcbfa88b3
bvsspavankumar <bvsspavankumar@gmail.com>
2020-12-26 12:23:05 +0530

    Adding user state to redux store. Closes #12

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Switch, Route } from 'react-router-dom'
 import {ToastContainer} from 'react-toastify'
+import {useDispatch} from 'react-redux'
 import 'react-toastify/dist/ReactToastify.css'
 
 import Login from './pages/auth/Login'
@@ -9,9 +10,28 @@ import RegisterComplete from './pages/auth/RegisterComplete'
 import Home from './pages/Home'
 import Header from './components/nav/Header'
 
+import {auth} from './firebase'
+
 import './App.css';
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    const unsubscribe = auth.onAuthStateChanged(async user=>{
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult()
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: {
+            // name: user.name,
+            email: user.email,
+            token: idTokenResult.token
+          }
+        })
+      }
+    })
+    return () => unsubscribe()
+  }, [])
   return (
     <>
     <Header />
