337db324a1b4e1df4a44557db4613415ab628dd3
bvsspavankumar <bvsspavankumar@gmail.com>
2020-12-26 19:08:28 +0530

    Adding middleware to auth route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,15 +1,15 @@
 const express = require('express')
 
 const {createOrUpdateUser} = require('../controllers/auth')
-
+const {authCheck} = require('../middlewares/auth')
 const router = express.Router()
 
-router.get('/', (req, res)=>{
+router.get('/', authCheck, (req, res)=>{
     res.json({
         data: 'api root'
     })
 })
 
-router.get('/create-or-update-user', createOrUpdateUser)
+router.post('/create-or-update-user', authCheck, createOrUpdateUser)
 
 module.exports = router
