b355cc4a676324fd685cb7605480163c7385321c
bvsspavankumar <bvsspavankumar@gmail.com>
2020-12-27 23:25:18 +0530

    Adding Category pages for create, list and remove. Closes #32

--- client/src/functions/category.js
+++ client/src/functions/category.js
@@ -1,28 +1,28 @@
 import axios from 'axios'
 
 export const getCategories = async () => {
-    await axios.get(`${process.env.REACT_APP_API}/categories`)
+    return await axios.get(`${process.env.REACT_APP_API}/category`)
 }
 
 export const getCategory = async (slug) => {
-    await axios.get(`${process.env.REACT_APP_API}/categories/${slug}`)
+    return await axios.get(`${process.env.REACT_APP_API}/category/${slug}`)
 }
 
 export const createCategory = async (category, authtoken) => {
-    await axios.post(`${process.env.REACT_APP_API}/categories`, 
-        { name: category },
+    return await axios.post(`${process.env.REACT_APP_API}/category`, 
+        category,
         { headers: {authtoken} }
     )
 }
 
 export const removeCategory = async (slug, authtoken) => {
-    await axios.delete(`${process.env.REACT_APP_API}/categories/${slug}`, {
+    return await axios.delete(`${process.env.REACT_APP_API}/category/${slug}`, {
         headers: { authtoken }
     })
 }
 
-export const updateCategory = async (slug) => {
-    await axios.pug(`${process.env.REACT_APP_API}/categories/${slug}`, 
+export const updateCategory = async (slug, category, authtoken) => {
+    return await axios.pug(`${process.env.REACT_APP_API}/categories/${slug}`, 
         { name: category },
         { headers: {authtoken} }
     )
--- client/src/pages/admin/category/CategoryCreate.js
+++ client/src/pages/admin/category/CategoryCreate.js
@@ -1,8 +1,89 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {toast} from 'react-toastify'
+import {useSelector} from 'react-redux'
+import {Link} from 'react-router-dom'
+import {EditOutlined, DeleteOutlined} from '@ant-design/icons'
 
+import {createCategory, getCategories, removeCategory} from '../../../functions/category'
 import AdminNav from '../../../components/nav/AdminNav'
 
-const AdminDashboard = () => {
+const CategoryCreate = () => {
+    const {user} = useSelector(state=>({...state}))
+    const [name, setName] = useState('')
+    const [loading, setLoading] = useState(false)
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        loadCategories()
+    }, [])
+
+    const loadCategories = () => 
+        getCategories().then(c=>setCategories(c.data))
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setLoading(true)
+        createCategory({name}, user.token)
+            .then(res=>{
+                setLoading(false)
+                setName('')
+                toast.success(`${res.data.name} is created`)
+                loadCategories()
+            })
+            .catch(err=>{
+                setLoading(false)
+                console.log(err)
+                toast.error('Error: ',err.response.data)
+            })
+    }
+
+    const handleRemove = (slug) => {
+        if(window.confirm("Delete?")) {
+            setLoading(true)
+            removeCategory(slug, user.token)
+            .then(res=>{
+                setLoading(false)
+                toast.info(`Removed ${slug}`)
+                loadCategories()
+            })
+        }
+
+    }
+    const categoryForm = (
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label>Name</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    onChange={(e)=>setName(e.target.value)}
+                    value={name}
+                    autoFocus
+                    required
+                />
+                <br />
+                <button className="btn btn-outline-primary">Save</button>
+            </div>
+        </form>
+    )
+
+    const renderCategories = () => categories.map((c)=>(
+        <div className='alert alert-secondary' key={c._id}>
+            {c.name} 
+            <span 
+                onClick={()=>handleRemove(c.slug)}
+                className="btn btn-sm float-right"
+            >
+                <DeleteOutlined className='text-danger' />
+            </span> 
+            <Link to={`/admin/category/${c.slug}`}>
+                <span className="btn btn-sm float-right">
+                    <EditOutlined />
+                </span> 
+            </Link>
+        </div>
+    ))
+    
 
     return (
         <div className="container-fluid">
@@ -10,10 +91,13 @@ const AdminDashboard = () => {
                 <AdminNav />
             </div>
             <div className="col">
-            Create category
+                {loading?<h4>Loading...</h4>:<h4>Create Category</h4>}
+                {categoryForm}
+                <hr />
+                {renderCategories()}
             </div>
         </div>
     )
 }
 
-export default AdminDashboard
+export default CategoryCreate
